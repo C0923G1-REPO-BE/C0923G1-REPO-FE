b9f6ffd35930fcea91e8b15188db3e4ef3ead677
Quốc Giàu <quocgiau61s@gmail.com>
2024-02-01 16:41:06 +0700

    first commit

---
+++ case_study_c09/src/main/java/com/example/case_study_c09/DTO/BookDTO.java
@@ -0,0 +1,4 @@
+package com.example.case_study_c09.DTO;
+
+public class BookDTO {
+}
---
+++ case_study_c09/src/main/java/com/example/case_study_c09/Service/GiauBookService.java
@@ -0,0 +1,7 @@
+package com.example.case_study_c09.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GiauBookService {
+}
