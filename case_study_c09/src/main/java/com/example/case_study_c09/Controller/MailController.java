bebd230997cc525fc0f142f29e1dbe5869e4b509
Quốc Giàu <quocgiau61s@gmail.com>
2024-02-21 15:51:07 +0700

    giau up

---
+++ case_study_c09/src/main/java/com/example/case_study_c09/Controller/MailController.java
@@ -0,0 +1,9 @@
+package com.example.case_study_c09.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class MailController {
+}
